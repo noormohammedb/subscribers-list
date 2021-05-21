db80176f454ec6a5990215b8a864f31ddd8c0168
Noor Mohammed <mail@noormohammed.tech>
2021-05-22 00:08:00 +0530

    logic progress

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+async function call() {
+  let rawData = await fetch(
+    "http://localhost:5500/YouTube and YouTube Music/subscriptions/subscriptions.json"
+  );
+  let data = await rawData.json();
+  console.log(data);
+}
+
+call();
